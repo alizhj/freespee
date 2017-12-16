6b5ecd333e32eae89975c92b89255e544c338ca7
alizhj <lisahjarpe@gmail.com>
2017-12-16 15:55:12 +0100

    functionality works

--- js/main.js
+++ js/main.js
@@ -1,3 +1,41 @@
 $( document ).ready(function() {
-  //$('.dialer').fadeOut();
+
+
+  $('.btn').on('click', function(e) {
+    e.preventDefault();
+    var target = $(this);
+    var value = target.attr('data-value');
+
+    var showInput = $('.result');
+    showInput.val( showInput.val() + value );
+  });
+
+
+  $('.submit').on('click', function(e) {
+    e.preventDefault();
+
+    //get value from input
+    var result = $('.result').val();
+
+    //split input value into array
+    var res = result.split('');
+
+    //remove all non number values from array
+    var cleanRes = res.filter(function(el) {
+      return el.length && el==+el;
+    });
+
+    var total = 0;
+    for (var i = 0; i < cleanRes.length; i++) {
+        total += cleanRes[i] << 0;
+    }
+    console.log(total);
+
+
+    // $.each(cleanRes, function (index, value) {
+    //   console.log(value);
+    // });
+  });
+
+
 });
